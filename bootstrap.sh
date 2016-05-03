1401718d0c48fa8a8c45ad320cefb9bdd1038ba4
Paul Sanwald <pcsanwald@gmail.com>
2016-05-03 11:36:48 -0700

    fixing stuff

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo xcodebuild -license
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install python
+brew install pyenv
+brew install vim
+brew install maven
+brew install virtualenv
+brew install pyenv-virtualenv
+brew install jq
+brew install heroku-toolbelt
+brew install ack
+brew install node
+brew install rabbitmq
+brew install redis
+brew install maven
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
